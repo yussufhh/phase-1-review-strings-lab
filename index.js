d664fc24a60fa87edd4ab310a17a683ae1db52aa
yussufhh <0793477632g@gmail.com>
2024-05-04 15:53:26 +0300

    complete_lab2_JS

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+const currentUser = 'Moon lover';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+const substringToCheck = "oon lover";
+
+
